149e328095d5a3a7608b0d03fc55765d2475d65a
Sector0880 <revampcroot0880@gmail.com>
2023-11-17 21:54:20 +0300

    up4

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
-const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+document.addEventListener("DOMContentLoaded", function() {
+    var ctx = document.getElementById("waveChart").getContext("2d");
+
+    var data = {
+    labels: ["30.05", "31.05", "01.06", "02.06", "03.06", "04.06", "05.06", "07.06", "08.06", "09.06"],
+    datasets: [
+        {
+        label: "Волна 1",
+        data: [12, 25, 40, 55, 42, 30, 18, 50, 38, 20],
+        backgroundColor: "rgba(54, 162, 235, 0.5)",
+        borderColor: "rgba(54, 162, 235, 1)",
+        borderWidth: 1,
+        fill: true
+        },
+        {
+        label: "Волна 2",
+        data: [30, 45, 20, 35, 50, 40, 28, 15, 32, 48],
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+        fill: true
         }
-      }
+    ]
+    };
+
+    var options = {
+    scales: {
+        y: {
+        beginAtZero: true,
+        max: 60
+        }
     }
-  });
+    };
+
+    var waveChart = new Chart(ctx, {
+    type: "line",
+    data: data,
+    options: options
+    });
+});
